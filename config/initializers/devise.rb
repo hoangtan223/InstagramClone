f701a72cc99a4379d33b4f9fd9c1efd1d6c1cd5b
Tanbui <hoangtan223@gmail.com>
2016-03-02 21:48:47 +0700

    Change Revise secret key to use ENV variable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,7 +7,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '0a4d15c908109642030a3cf4525839eb52aba49491f96941b77fc614b42b6b6ff9e45b050631c7d8030b8ea3a1c51976113f67544c458868f2ff675bc9b7cd97'
-
+  config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env.production?
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
