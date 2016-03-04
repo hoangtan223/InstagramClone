1258ca7b2491f12c92cbf894ac455265ad35288d
Tanbui <hoangtan223@gmail.com>
2016-03-04 20:58:13 +0700

    Fix bug UI for delete comment

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
       data = response.responseJSON;
       if (data.success){
       	var comment_list = $(this).parent().prev();
-      	var newComment = "<li class=\"row\"><a href=\"/user/" + data.user_id + "\" class=\"col-xs-1\" username>"+ data.username +"</a><div class=\"col-xs-9\"> " + data.comment +"</div></li>"
+      	var newComment = "<li class=\"row\"><a href=\"/user/" + data.user_id + "\" class=\"col-xs-2 username\" >"+ data.username +"</a><div class=\"col-xs-8\"> " + data.comment +"</div></li>"
         comment_list.append(newComment);
         $(this).find("#comment_comment").val("");
         $(this).find("#comment_comment").blur();
